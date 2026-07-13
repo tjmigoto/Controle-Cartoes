b487e30959fe136a8d68b5c44ab29d9ea6489b9a
tjmigoto <135332333+tjmigoto@users.noreply.github.com>
2026-07-13 11:16:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,36 @@
-const CACHE_NAME = "controle-cartoes-v1";
+const CACHE_NAME = "controle-cartoes-v3";
+
+const ARQUIVOS_PARA_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARQUIVOS_PARA_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((nomes) =>
+      Promise.all(nomes.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
+    caches.match(event.request).then((respostaCache) => {
+      return (
+        respostaCache ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
+    })
   );
 });
